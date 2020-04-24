5fe37edea2166791acbce033f5a35c9e181428ab
yatXuan <yatxuan@163.com>
2020-04-24 16:38:20 +0800

    MySQL分库分表实现

--- yi_xuan_sub_database_Table/src/main/resources/SQL/lab18_orders_0.sql
+++ yi_xuan_sub_database_Table/src/main/resources/SQL/lab18_orders_0.sql
@@ -16,16 +16,6 @@ Date: 2020-04-24 16:28:05
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for orders
--- ----------------------------
-DROP TABLE IF EXISTS `orders`;
-CREATE TABLE `orders` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '订单编号',
-  `user_id` int(16) DEFAULT NULL COMMENT '用户编号',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='订单表';
-
--- ----------------------------
 -- Records of orders
 -- ----------------------------
 
