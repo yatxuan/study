1e64418594b62da5399f609c9b5704693d684cef
yatXuan <yatxuan@163.com>
2020-07-29 11:17:54 +0800

    整合netty-未完成

---
+++ yi_xuan_netty/src/main/java/com/yat/NettyApplication.java
@@ -0,0 +1,19 @@
+package com.yat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>Description: 描述 </p>
+ *
+ * @author Yat-Xuan
+ * @version 1.0
+ * @date 2020/7/29 - 9:33
+ */
+@SpringBootApplication
+public class NettyApplication   {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyApplication.class);
+    }
+}
