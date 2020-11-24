693c8ebf3b36d7aa3d1eb9e0023a9c582429966a
yatXuan <yatxuan@163.com>
2020-11-24 15:52:38 +0800

    删除多余代码

--- split_configuration_file/src/main/java/com/yat/split/file/SplitFileApplication.java
+++ split_configuration_file/src/main/java/com/yat/split/file/SplitFileApplication.java
@@ -1,7 +1,6 @@
 package com.yat.split.file;
 
 import com.yat.split.file.common.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
